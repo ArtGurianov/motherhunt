e524962ac5a3a510819a9fd279a5622b96224fd1
Art <artgurianov@yandex.ru>
2025-06-13 11:51:49 +0700

    404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+import { PageSection } from "@/components/PageSection";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <PageSection className="flex grow justify-center items-center">
+      <div className="w-full max-w-sm flex flex-col justify-center items-center">
+        <Image
+          src="/404.png"
+          width="0"
+          height="0"
+          sizes="100vw"
+          alt="logo"
+          className="h-auto w-full shrink-0"
+          priority
+        />
+        <span className="text-lg text-foreground/90">
+          {"Got lost? "}
+          <Link className="underline" href="/">
+            {"Just go home"}
+          </Link>
+        </span>
+      </div>
+    </PageSection>
+  );
+}
