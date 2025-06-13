392bbff1677a9f506a41f0acf1a36b9492d672e7
Art <artgurianov@yandex.ru>
2025-06-13 18:02:24 +0700

    mother agencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Marquee } from "@/components/Marquee";
 import { PageSection } from "@/components/PageSection";
 import { Quote } from "@/components/Quote/Quote";
 import HeroAnimationSvgUrl from "@/components/svg/HeroAnimation.svg?url";
@@ -36,7 +37,7 @@ export default async function Home() {
   }
 
   return (
-    <div className="flex flex-col gap-6 grow justify-start items-center pb-12">
+    <div className="flex flex-col gap-12 grow justify-start items-center pb-12">
       <PageSection className="flex flex-col lg:flex-row w-full justify-center items-center">
         <div className="w-full lg:w-1/2 flex flex-col gap-8 justify-center items-center py-8">
           <h1 className="text-5xl text-center">
@@ -73,9 +74,9 @@ export default async function Home() {
         </div>
       </PageSection>
       <PageSection className="flex flex-col grow justify-start items-center">
-        <Card className="w-full max-w-lg pt-0 overflow-clip">
+        <Card className="w-full max-w-lg pt-0">
           <CardTitle className="bg-linear-to-tr from-accent-foreground to-accent-foreground/50 border-b-2 py-2">
-            <CardHeader className="text-4xl font-bold text-center">
+            <CardHeader className="text-4xl font-bold text-center text-foreground/95">
               {"🛸 Our Mission ✨"}
             </CardHeader>
           </CardTitle>
@@ -115,12 +116,57 @@ export default async function Home() {
           </CardContent>
         </Card>
       </PageSection>
-      <PageSection className="flex flex-col grow justify-start items-center">
-        <ul>
-          {agencies.map(({ name }) => (
-            <li key={name}>{name}</li>
-          ))}
-        </ul>
+      <PageSection fullWidth>
+        <h2 className="text-3xl font-medium font-mono px-4 mb-1 text-foreground/90">
+          {"Partnered with:"}
+        </h2>
+        <Marquee>
+          <div className="bg-primary bg-[linear-gradient(to_right,#FF00F9,transparent_1px),linear-gradient(to_bottom,#FF00F9,transparent_1px)] bg-[size:70px_70px] py-2">
+            <div className="flex flex-col justify-start items-center border-background border-y-12 border-dashed py-2">
+              <div className="flex gap-12 pl-12">
+                {agencies.map((agency) => (
+                  <Button
+                    asChild
+                    variant="ghost"
+                    size="reset"
+                    key={agency.name}
+                    className="h-48 w-64 relative shrink-0"
+                  >
+                    <Link
+                      href={agency.url}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <Image
+                        key={agency.name}
+                        alt={`logo image for ${agency.name} agency`}
+                        className="h-full w-full object-contain"
+                        src={`https:${agency.logoUrl}`}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        priority
+                      />
+                    </Link>
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Marquee>
+        <p className="flex justify-center items-center text-2xl font-mono mt-2 flex-wrap gap-x-4">
+          <span className="font-semibold text-foreground/90">
+            {"Are you a Mother Agent?"}
+          </span>
+          <span>
+            <Button variant="secondary" className="text-xl font-mono" asChild>
+              <Link href="/apply">{"Apply here"}</Link>
+            </Button>
+            <span className="ml-4 font-semibold text-foreground/90">
+              {"to join!"}
+            </span>
+          </span>
+        </p>
       </PageSection>
     </div>
   );
